55d2883331ec3842755485d7ddc1cf9fbe4631ca
Bernd Kampl <bernd@itranslate.com>
2021-11-23 09:43:56 +0100

    Enable DataBinding, add ViewModel

--- app/src/main/java/com/bitmovin/trialday/MainActivity.kt
+++ app/src/main/java/com/bitmovin/trialday/MainActivity.kt
@@ -2,12 +2,21 @@ package com.bitmovin.trialday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.bitmovin.trialday.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
     }
 }
